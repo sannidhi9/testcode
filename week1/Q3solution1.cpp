33777ab70210223b05e8af1e87ee212e52477839
sannidhi9 <38998613+sannidhi9@users.noreply.github.com>
2019-02-17 17:26:18 +0530

    issues fixed

--- week1/Q3solution1.cpp
+++ week1/Q3solution1.cpp
@@ -8,7 +8,7 @@ Can you do it without creating a new array?
 using namespace std;
 int main()
 {
-	int arr[50],arr1[50],len,i,j=0,c=0;
+	int arr[50],len,i,j,count0=0;
 	cout<<"Enter array size : ";
 	cin>>len;
 	cout<<"Enter array elements : ";
@@ -16,21 +16,26 @@ int main()
 	{
 		cin>>arr[i];
 	}
-	for(i=0; i<len; i++)
-	{
-		if(arr[i]!=0)
-		{
-			arr1[j]=arr[i];
-			j++;
-		}
-	}
-	for(i=j;i<len;i++)
-       arr1[i]=0;
+
+	for(i=0;i<len;i++)
+    {
+        if(arr[i]==0)
+        {
+            count0++;
+            for(j=i+1;j<len;j++)
+            {
+                arr[j-1]=arr[j];
+            }
+        }
+    }
+
+
+
 
 		cout<<"Now the new array is :\n";
 		for(i=0; i<len; i++)
 		{
-			cout<<arr1[i]<<" ";
+			cout<<arr[i]<<" ";
 		}
 	return 0;
 }
